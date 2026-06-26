97aa4b96ffe0944f2151b894bda8b909039ca6ee
mayureshzende <mayureshzende@gmail.com>
2026-06-26 14:28:30 +0400

    adding the longest substring problem

---
+++ strings/longest_subString/optimalSol.js
@@ -0,0 +1,31 @@
+/**
+ * time complexity: O (n ^ 2 )
+ * space complexity: O (n)
+ */
+
+const s = "abcadcbb";
+//         l
+//         r
+// const s = "pwwkew";
+// const s = "abcabcbb";
+
+function getLongestSubstring(str) {
+  if (str.length < 1) return str.length;
+
+  let longest = 0;
+  let left = 0;
+  const seenChars = {};
+  for (let right = 0; right < str.length; right++) {
+    let currentChar = str[right];
+    let prevSeenChar = seenChars[currentChar];
+    if (prevSeenChar >= left) {
+      left = prevSeenChar + 1;
+    }
+    seenChars[currentChar] = right;
+    longest = Math.max(longest, right - left + 1);
+  }
+
+  return longest;
+}
+
+console.log(getLongestSubstring(s));
